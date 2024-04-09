d60795e9815296161d2c7bce9c0b6e6ef4771e46
kensuke-0n0 <kensuke.n9925@gmail.com>
2024-04-09 19:56:37 +0900

    Initial Commit

---
+++ XSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSampleApp
+//
+//  Created by にしいけんすけ on 2024/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
