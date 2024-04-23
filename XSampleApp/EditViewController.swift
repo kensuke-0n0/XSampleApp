1c0258456a03920c95907d6ef9f2143e5b7756d6
kensuke-0n0 <kensuke.n9925@gmail.com>
2024-04-24 00:15:59 +0900

    UITextFieldとUITextViewのそれぞれにプレースホルダーをつけた

--- XSampleApp/EditViewController.swift
+++ XSampleApp/EditViewController.swift
@@ -9,12 +9,25 @@ import UIKit
 
 class EditViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let placeholderText = "いまどうしてる？"
+    let placeholderTextField = "ユーザー名を入力してください"
+    
+    // MARK: - IBOutlets
+    
     @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var tweetTextView: UITextView!
     
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureTextView()
     }
+    
+    // MARK: - IBActions
     
     @IBAction func didTapCancelButton(_ sender: Any) {
         // 前の画面に戻る
@@ -24,4 +37,14 @@ class EditViewController: UIViewController {
     @IBAction func didTapPostButton(_ sender: Any) {
     }
     
+    // MARK - Other Methods
+    
+    func configureTextView() {
+        // プレースホルダーテキストを設定
+        tweetTextView.text = placeholderText
+        tweetTextView.textColor = UIColor.lightGray
+        
+        userNameTextField.text = placeholderTextField
+        userNameTextField.textColor = UIColor.lightGray
+    }
 }
